84d853d75550959c17ddbc6d1e76a51b242100af
Ibrahim Khawar <ikb1703@gmail.com>
2022-01-27 19:51:07 -0500

    Header rendering

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -5,7 +5,12 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { UserState } from "../../types";
 
-const Header: React.FC = () => {
+interface HeaderProps {
+	Middle?: any;
+	Right?: any;
+}
+
+const Header: React.FC<HeaderProps> = (props) => {
 	const userState: UserState = useSelector((state: any) => state.userReducer);
 	const navigate = useNavigate();
 
@@ -19,9 +24,17 @@ const Header: React.FC = () => {
 
 	return (
 		<div id='header-container'>
-			<div id='header-logo-container' onClick={handleHeaderLogoClick}>
-				<Logo id='header-logo' dark={false} scale={1} />
-				<h1 id='header-logo-text'>LOGISENSE</h1>
+			<div id='header-content'>
+				<div id='header-logo-container' onClick={handleHeaderLogoClick}>
+					<Logo id='header-logo' dark={false} scale={1} />
+					<h1 id='header-logo-text'>LOGISENSE</h1>
+				</div>
+				{props.Middle && (
+					<div id='header-middle'>{props.Middle && props.Middle}</div>
+				)}
+				{props.Right && (
+					<div id='header-right'>{props.Right && props.Right}</div>
+				)}
 			</div>
 		</div>
 	);
--- client/src/pages/admin/ProjectsPage/ProjectsPage.tsx
+++ client/src/pages/admin/ProjectsPage/ProjectsPage.tsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { UserState } from "../../../types";
+import Input from "../../../components/Form/Input";
 import Header from "../../../components/Header/Header";
 
 const ProjectsPage: React.FC = () => {
+	const [searchQuery, setSearchQuery] = useState("");
+	const [projects, setProjects] = useState([]);
+
 	const userState: UserState = useSelector((state: any) => state.userReducer);
 
 	return (
 		<div className='page-with-header' id='admin-projects-page'>
-			<Header />
-			<h1>PROJECTS PAGE</h1>
-			<p>YAY</p>
-			<p>{userState.userEmail}</p>
+			<Header
+				Middle={
+					<Input
+						id='projects-page-header-input'
+						value={searchQuery}
+						placeholder='Search'
+						update={setSearchQuery}
+					/>
+				}
+			/>
 		</div>
 	);
 };
